36b1800dbc5e4d13e338982c92d15101a13173f3
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2025-08-03 22:14:42 -0300

    aula 09

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello IA Bootcamp")
+# nome = input('Digite seu nome: ')
+# idade = int(input('Digite sua idade: '))
+# idadeAutorizada = int(20)
+# if idade < idadeAutorizada:
+#     # print(nome, 'você é menor de idade.')
+#     print(f'Olá {nome}, sua idade é {idade} e você não está autorizado para acessar esse conteúdo.')
+# else:
+#     # print('Olá', nome, 'sua idade é', idade, 'e você é maior de idade.')
+#     print(f'Olá {nome}, sua idade é {idade + int(5)} e você pode acessar o conteúdo.')
+# print('Obrigado por participar!')
+
+porcoes_total = int(input('Digite a quantidade de porções: '))
+porcoes_dias  = int(input('Digite a quantidade de porções consumidas por dia: '))
+
+dias = porcoes_total // porcoes_dias
+print(f'Você poderá consumir as porções por {dias} dias.')
