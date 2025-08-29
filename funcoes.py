44b18e70a494ad927f0234491feae55b4fbf82bf
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2025-08-28 23:04:13 -0300

    MaiorIdade

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,8 @@
+def maiorIdade(idade):
+    if idade >= 18:
+        return "Muito bem, você já é maior de idade"
+    else 
+        return "Você ainda não pode, é menor de idade"
 
 def soma(a, b):
     return a + b
